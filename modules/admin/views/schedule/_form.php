f33cb8b7ebb89529ade14a5fa159d47fc31fbb11
DaemonNikolay <nikolay-eckert@mail.ru>
2018-05-03 19:16:11 +0700

    Small correct

--- modules/admin/views/schedule/_form.php
+++ modules/admin/views/schedule/_form.php
@@ -12,59 +12,72 @@ use app\models\Groups;
 ?>
 
 <div class="schedule-form">
-    <?php $form = ActiveForm::begin(); ?>
-
+    <?php $form = ActiveForm::begin([
+                                        'options' => [
+                                            'style' => 'width: 100%;',
+                                            'autocomplete' => 'on',
+                                            ]
+                                    ]); ?>
 
 
     <?php $model->id = Yii::$app->user->id; ?>
-<!---->
-<!--    --><?php //var_dump($model->id) ;
-//    exit;
-//
-//    ?>
 
-
-
-
-
-    <?= $form->field($model, 'id')->dropDownList(ArrayHelper::map($groups, 'id', 'name'))->label('Выберите группу'); ?>
+    <?= $form->field($model, 'id')
+             ->dropDownList(ArrayHelper::map($groups, 'id', 'name'))
+             ->label('Выберите группу'); ?>
 
 
 
 
 
 
-    <?= $form->field($model, 'for_the_day')->textInput() ?>
+    <?= $form->field($model, 'for_the_day')
+             ->textInput() ?>
 
-    <?= $form->field($model, 'for_the_group')->textInput() ?>
+    <?= $form->field($model, 'for_the_group')
+             ->textInput() ?>
 
-    <?= $form->field($model, 'session_1_discipline')->textInput() ?>
+    <?= $form->field($model, 'session_1_discipline')
+             ->textInput() ?>
 
-    <?= $form->field($model, 'session_1_teacher')->textInput() ?>
+    <?= $form->field($model, 'session_1_teacher')
+             ->textInput() ?>
 
-    <?= $form->field($model, 'session_2_discipline')->textInput() ?>
+    <?= $form->field($model, 'session_2_discipline')
+             ->textInput() ?>
 
-    <?= $form->field($model, 'session_2_teacher')->textInput() ?>
+    <?= $form->field($model, 'session_2_teacher')
+             ->textInput() ?>
 
-    <?= $form->field($model, 'session_3_discipline')->textInput() ?>
+    <?= $form->field($model, 'session_3_discipline')
+             ->textInput() ?>
 
-    <?= $form->field($model, 'session_3_teacher')->textInput() ?>
+    <?= $form->field($model, 'session_3_teacher')
+             ->textInput() ?>
 
-    <?= $form->field($model, 'session_4_discipline')->textInput() ?>
+    <?= $form->field($model, 'session_4_discipline')
+             ->textInput() ?>
 
-    <?= $form->field($model, 'session_4_teacher')->textInput() ?>
+    <?= $form->field($model, 'session_4_teacher')
+             ->textInput() ?>
 
-    <?= $form->field($model, 'session_5_discipline')->textInput() ?>
+    <?= $form->field($model, 'session_5_discipline')
+             ->textInput() ?>
 
-    <?= $form->field($model, 'session_5_teacher')->textInput() ?>
+    <?= $form->field($model, 'session_5_teacher')
+             ->textInput() ?>
 
-    <?= $form->field($model, 'session_6_discipline')->textInput() ?>
+    <?= $form->field($model, 'session_6_discipline')
+             ->textInput() ?>
 
-    <?= $form->field($model, 'session_6_teacher')->textInput() ?>
+    <?= $form->field($model, 'session_6_teacher')
+             ->textInput() ?>
 
-    <?= $form->field($model, 'name_room')->textInput() ?>
+    <?= $form->field($model, 'name_room')
+             ->textInput() ?>
 
-    <?= $form->field($model, 'updated_at')->textInput() ?>
+    <?= $form->field($model, 'updated_at')
+             ->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
