4ae1cd03140cc74d68ab9bc225b5eae46c8cbac0
DaemonNikolay <nikolay-eckert@mail.ru>
2018-05-19 17:23:29 +0700

    Create show schedule for guests

--- controllers/ScheduleController.php
+++ controllers/ScheduleController.php
@@ -4,35 +4,20 @@ namespace app\controllers;
 
 use app\models\Groups;
 use app\models\Schedule;
+use app\models\ScheduleSearch;
+use Yii;
 
 class ScheduleController extends \yii\web\Controller
 {
     public function actionIndex()
     {
         $model = Schedule::find()->all();
+        $groups = Groups::find()->all();
+        $searchModel = new ScheduleSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
-
-
-
-
-//        $groups = Groups::find()->all();
-
-
-        echo '<pre>';
-        foreach ($model as $item){
-//            var_dump($item->for_the_group);
-//            echo '<br /><br /><br /><br /><br />';
-
-            $groups = Groups::find()->where(['id' => $item->for_the_group])->all();
-            echo $groups[0]->name . ' | ';
-
-
-
-        }
-        echo '</pre>';
-
-        exit();
-        return $this->render('index', ['model' => $model, 'groups' => $groups]);
+        return $this->render('index', ['model' => $model, 'groups' => $groups, 'dataProvider' => $dataProvider,
+                                       'searchModel' => $searchModel,]);
     }
 
 }
--- views/schedule/index.php
+++ views/schedule/index.php
@@ -6,9 +6,13 @@ use app\assets\AppAsset;
 use yii\widgets\ActiveForm;
 use yii\jui\DatePicker;
 use yii\helpers\ArrayHelper;
+use yii\grid\GridView;
+use app\models\Groups;
 
 /* @var $groups app\controllers\ScheduleController */
 /* @var $model app\controllers\ScheduleController */
+/* @var $searchModel app\models\ScheduleSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Расписание';
 $this->params['breadcrumbs'][] = $this->title;
@@ -17,13 +21,13 @@ $this->params['breadcrumbs'][] = $this->title;
 <h1><?= Html::encode($this->title) ?></h1>
 <?php
 
-echo '<pre>';
-foreach ($model as $item){
-    var_dump( $item->for_the_group);
-//    $groups->id = $item->for_the_group;
-
-}
-echo '</pre>';
+//echo '<pre>';
+//foreach ($model as $item){
+//    var_dump( $item->for_the_group);
+////    $groups->id = $item->for_the_group;
+//
+//}
+//echo '</pre>';
 
 
 
@@ -89,6 +93,65 @@ echo '</pre>';
 //                      ]
 //                  ]);
 ?>
+
+
+<?= GridView::widget(['dataProvider' => $dataProvider, 'filterModel' => $searchModel, 'columns' => [
+
+
+    ['attribute' => 'for_the_day', 'label' => 'День',
+     'filter' => DatePicker::widget(['model' => $searchModel, 'value' => $searchModel->for_the_day,
+                                     'attribute' => 'for_the_day', 'options' => ['class' => 'form-control'],
+                                     'language' => 'ru', 'dateFormat' => 'yyyy-MM-dd',]),
+     'format' => ['date', 'l yyyy-MM-dd'], 'options' => ['width' => '170']],
+
+
+    ['attribute' => 'for_the_group', 'label' => 'Группа',
+     'filter' => ArrayHelper::map(Groups::find()->all(), 'id', 'name'),
+     'filterInputOptions' => ['class' => 'form-control form-control-sm'], 'value' => 'forTheGroup.name',
+     'options' => ['width' => '130']],
+
+    ['label' => '№ пары / Дисциплина / преподаватель / аудитория / время',
+     'format' => 'raw', 'value' => function ($model)
+    {
+        $session_1 = '1 / ';
+        $session_1 .= $model['session1Discipline']->name . ' / ';
+        $session_1 .= $model['session1Teacher']->name . ' ' . mb_strimwidth($model['session1Teacher']->patronymic, 0, 1) . '. ' . mb_strimwidth($model['session1Teacher']->surname, 0, 1) . '. / ';
+        $session_1 .= $model['session1Room']->name . ' (' . $model['session1Room']->type . ')<br>';
+
+        $session_2 = '2 / ';
+        $session_2 .= $model['session2Discipline']->name . ' / ';
+        $session_2 .= $model['session2Teacher']->name . ' ' . mb_strimwidth($model['session2Teacher']->patronymic, 0, 1) . '. ' . mb_strimwidth($model['session2Teacher']->surname, 0, 1) . '. / ';
+        $session_2 .= $model['session2Room']->name . ' (' . $model['session2Room']->type . ')<br>';
+
+        $session_3 = '3 / ';
+        $session_3 .= $model['session3Discipline']->name . ' / ';
+        $session_3 .= $model['session3Teacher']->name . ' ' . mb_strimwidth($model['session3Teacher']->patronymic, 0, 1) . '. ' . mb_strimwidth($model['session3Teacher']->surname, 0, 1) . '. / ';
+        $session_3 .= $model['session3Room']->name . ' (' . $model['session3Room']->type . ')<br>';
+
+        $session_4 = '4 / ';
+        $session_4 .= $model['session4Discipline']->name . ' / ';
+        $session_4 .= $model['session4Teacher']->name . ' ' . mb_strimwidth($model['session4Teacher']->patronymic, 0, 1) . '. ' . mb_strimwidth($model['session4Teacher']->surname, 0, 1) . '. / ';
+        $session_4 .= $model['session4Room']->name . ' (' . $model['session4Room']->type . ')<br>';
+
+        $session_5 = '5 / ';
+        $session_5 .= $model['session5Discipline']->name . ' / ';
+        $session_5 .= $model['session5Teacher']->name . ' ' . mb_strimwidth($model['session5Teacher']->patronymic, 0, 1) . '. ' . mb_strimwidth($model['session5Teacher']->surname, 0, 1) . '. / ';
+        $session_5 .= $model['session5Room']->name . ' (' . $model['session5Room']->type . ')<br>';
+
+        $session_6 = '6 / ';
+        $session_6 .= $model['session6Discipline']->name . ' / ';
+        $session_6 .= $model['session6Teacher']->name . ' ' . mb_strimwidth($model['session6Teacher']->patronymic, 0, 1) . '. ' . mb_strimwidth($model['session6Teacher']->surname, 0, 1) . '. / ';
+        $session_6 .= $model['session6Room']->name . ' (' . $model['session6Room']->type . ')<br>';
+
+        $return = $session_1;
+        $return .= $session_2;
+        $return .= $session_3;
+        $return .= $session_4;
+        $return .= $session_5;
+        $return .= $session_6;
+
+        return $return;
+    },],],]); ?>
 
 <?php $this->registerCssFile('/web/css/schedule-guest.css', ['depends' => [AppAsset::className()]]) ?>
 
