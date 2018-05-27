e7bd81271e3349537252684230541c18a77aa9e9
DaemonNikolay <nikolay-eckert@mail.ru>
2018-05-28 01:06:57 +0700

    Upgrade page 404

--- views/site/error.php
+++ views/site/error.php
@@ -11,17 +11,30 @@ $this->title = $name;
 ?>
 <div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
-    </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
+    <section id="error-page">
+        <div class="error-page-inner">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="text-center">
+                            <div class="bg-404">
+                                <div class="error-image">
+                                    <img class="img-responsive" src="/web/assets_main/images/404.png" alt="">
+                                </div>
+                            </div>
+                            <h2>Страница не найдена</h2>
+                            <p>Oooops...</p>
+                            <a href="/" class="btn btn-error">Вернуться на главную</a>
+                            <div class="social-link">
+                                <span><a href="#"><i class="fa fa-vk"></i></a></span>
+                                <span><a href="#"><i class="fa fa-twitter"></i></a></span>
+                                <span><a href="#"><i class="fa fa-youtube"></i></a></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 </div>
